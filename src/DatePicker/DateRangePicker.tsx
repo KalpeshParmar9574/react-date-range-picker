a157c318cc84fecb3b9e5729db9c15481101ca01
Sunil Sarvaiya <sarvaiyasunil01@gmail.com>
2024-05-12 17:55:46 +0530

    fixed design issue and format the code

--- src/DatePicker/DateRangePicker.tsx
+++ src/DatePicker/DateRangePicker.tsx
@@ -1,69 +1,101 @@
 // DateRangePicker.tsx
-import React, { useEffect, useState } from 'react';
-import { StaticDateRangeData } from '../assets/StaticDateRangeData';
-import { DateRangePickerProps, DateListOption } from '../interface/DatePickerPropsInterface';
-import { addDays, addYears, compareAsc, endOfMonth, format, startOfMonth, startOfYear, subMonths, subYears } from 'date-fns';
-import { Dropdown } from 'primereact/dropdown';
-import { Calendar } from 'primereact/calendar';
-import 'primeicons/primeicons.css';
-import { PrimeReactProvider } from 'primereact/api';
-        
+import React, { useEffect, useState } from "react";
+import { StaticDateRangeData } from "../assets/StaticDateRangeData";
+import {
+    DateRangePickerProps,
+    DateListOption,
+} from "../interface/DatePickerPropsInterface";
+import {
+    addDays,
+    addYears,
+    compareAsc,
+    endOfMonth,
+    format,
+    startOfMonth,
+    startOfYear,
+    subMonths,
+    subYears,
+} from "date-fns";
+import { Dropdown } from "primereact/dropdown";
+import { Calendar } from "primereact/calendar";
+import "primeicons/primeicons.css";
+import { PrimeReactProvider } from "primereact/api";
 
-const DateRangePicker: React.FC<DateRangePickerProps> = ({ selectedOptionIndex, onDateSelectionChanged, dateListOptions, selectedDates, dateFormat, minDate, maxDate, }) => {
-    const defaultMinDate = minDate ||  subYears(Date(), 10)
-    const defaultMaxDate = maxDate || addYears(Date(), 10)
+const DateRangePicker: React.FC<DateRangePickerProps> = ({
+    selectedOptionIndex,
+    onDateSelectionChanged,
+    dateListOptions,
+    selectedDates,
+    dateFormat,
+    minDate,
+    maxDate,
+}) => {
+    const defaultMinDate = minDate || subYears(Date(), 10);
+    const defaultMaxDate = maxDate || addYears(Date(), 10);
     const [isDatePickerVisible, setIsDatePickerVisible] = useState(false);
-    const [selectedDateRange, setselectedDateRange] = useState(StaticDateRangeData[selectedOptionIndex || 0]);
+    const [selectedDateRange, setselectedDateRange] = useState(
+        StaticDateRangeData[selectedOptionIndex || 0]
+    );
     const [dates, setDates] = useState<(Date | null)[]>(selectedDates || []);
-    const [dateStringLabel, setDateStringLabel] = useState("")
+    const [dateStringLabel, setDateStringLabel] = useState("");
     const handleSelectedDateOption = (value: DateListOption) => {
-        setselectedDateRange(value)
-    }
+        setselectedDateRange(value);
+    };
     useEffect(() => {
         if (selectedDateRange.dateDiff) {
-            const updatedDates = calculateDates(selectedDateRange.dateDiff, null)
-            setDates(updatedDates)  
-            const dateString = convertDateToString(updatedDates as [Date | null, Date | null]);
-            setDateStringLabel(dateString)
-        } else if(selectedDateRange.optionLabel === "Custom Range"){
-                setIsDatePickerVisible(true);
-                setDates([])
-        }else{
-            const updatedDates = calculateDates(null, selectedDateRange.optionLabel)
-            setDates(updatedDates)
-            const dateString = convertDateToString(updatedDates as [Date | null, Date | null]);
-            setDateStringLabel(dateString)
+            const updatedDates = calculateDates(selectedDateRange.dateDiff, null);
+            setDates(updatedDates);
+            const dateString = convertDateToString(
+                updatedDates as [Date | null, Date | null]
+            );
+            setDateStringLabel(dateString);
+        } else if (selectedDateRange.optionLabel === "Custom Range") {
+            setIsDatePickerVisible(true);
+            setDates([]);
+        } else {
+            const updatedDates = calculateDates(null, selectedDateRange.optionLabel);
+            setDates(updatedDates);
+            const dateString = convertDateToString(
+                updatedDates as [Date | null, Date | null]
+            );
+            setDateStringLabel(dateString);
         }
     }, [selectedDateRange]);
     useEffect(() => {
-        if(selectedDateRange.optionLabel === "Custom Range"){
-            const dateString =  convertDateToString(dates as [Date | null, Date | null])
-            setDateStringLabel(dateString)
-            if( dates && dates[1]){
-                setIsDatePickerVisible(false)
+        if (selectedDateRange.optionLabel === "Custom Range") {
+            const dateString = convertDateToString(
+                dates as [Date | null, Date | null]
+            );
+            setDateStringLabel(dateString);
+            if (dates && dates[1]) {
+                setIsDatePickerVisible(false);
             }
         }
         setselectedDateRange(selectedDateRange);
-        returnValues()
-    },[dates])
-    const convertDateToString = (dates: [Date | null, Date| null]) => {
-            const startDate = dates[0]
-            const endDate = dates[1];
-            let dateString: string;
-            if(endDate){
-                 dateString = `${format(startDate || Date(), dateFormat || 'dd/MM/yyyy')} - ${format(endDate || Date(),  dateFormat || 'dd/MM/yyyy')}`
-                 return dateString
-            }else{
-                 dateString = `${format(startDate || Date(),dateFormat ||  'dd/MM/yyyy')}`
-                 return dateString
-            }
-    }
+        returnValues();
+    }, [dates]);
+    const convertDateToString = (dates: [Date | null, Date | null]) => {
+        const startDate = dates[0];
+        const endDate = dates[1];
+        let dateString: string;
+        if (endDate) {
+            dateString = `${format(
+                startDate || Date(),
+                dateFormat || "dd/MM/yyyy"
+            )} - ${format(endDate || Date(), dateFormat || "dd/MM/yyyy")}`;
+            return dateString;
+        } else {
+            dateString = `${format(startDate || Date(), dateFormat || "dd/MM/yyyy")}`;
+            return dateString;
+        }
+    };
     const calculateDates = (diff: number | null, dateLabel: string | null) => {
         const currDate = new Date();
         if (diff) {
-
-            const nextDate = addDays(currDate, diff)
-            return compareAsc(currDate, nextDate) ? [nextDate, currDate] : [currDate, nextDate];
+            const nextDate = addDays(currDate, diff);
+            return compareAsc(currDate, nextDate)
+                ? [nextDate, currDate]
+                : [currDate, nextDate];
         } else if (dateLabel) {
             let currDate = new Date();
             let nextDate: Date | null;
@@ -88,14 +120,18 @@ const DateRangePicker: React.FC<DateRangePickerProps> = ({ selectedOptionIndex, 
                 default:
                     return [currDate, currDate];
             }
-            return nextDate ? compareAsc(currDate, nextDate) ? [nextDate, currDate] : [currDate, nextDate] : [currDate, null];
+            return nextDate
+                ? compareAsc(currDate, nextDate)
+                    ? [nextDate, currDate]
+                    : [currDate, nextDate]
+                : [currDate, null];
         } else {
-            return [currDate, currDate]
+            return [currDate, currDate];
         }
-    }
+    };
     const returnValues = () => {
-        let userSelectedDates: any = dates || [null, null]
-        onDateSelectionChanged(userSelectedDates, selectedDateRange)
+        let userSelectedDates: any = dates || [null, null];
+        onDateSelectionChanged(userSelectedDates, selectedDateRange);
         if (dateListOptions !== undefined) {
             StaticDateRangeData.map((item: DateListOption) => {
                 if (item.optionLabel === selectedDateRange.optionLabel) {
@@ -105,56 +141,67 @@ const DateRangePicker: React.FC<DateRangePickerProps> = ({ selectedOptionIndex, 
                 return item; // Return the modified item
             });
         }
-    }
+    };
     const handleUpdateDate = (event: any) => {
-        setDates(event)
-    }
+        setDates(event);
+    };
 
-    const handleClear = (event:any) =>{
+    const handleClear = (event: any) => {
         event.stopPropagation();
-        setDates([])
+        setDates([]);
         // selectedDateRange.selectedDate="";
-        setDateStringLabel("")
-        setIsDatePickerVisible(false)
-    }
-    const dropdownValueTemplate = (option:any) => {
+        setDateStringLabel("");
+        setIsDatePickerVisible(false);
+    };
+    const dropdownValueTemplate = (option: any) => {
         return (
-            <div className="flex"  style={{ cursor: 'pointer', marginLeft: 'auto' }}>
-               {dateStringLabel}
-                <i className="pi pi-times flex-end" onClick={(e) => handleClear(e)} style={{ cursor: 'pointer', marginLeft: 'auto' }}></i>
-                
-                <i className="pi pi-calendar ml-2 flex-end" style={{ fontSize: '1.2rem' }}></i>
+            <div className="flex">
+                {dateStringLabel}
+                <i
+                    className="pi pi-times cursor-pointer ml-2"
+                    onClick={(e) => handleClear(e)}
+                ></i>
+                <i
+                    className="pi pi-calendar ml-2 flex-end"
+                    style={{ fontSize: "1rem" }}
+                ></i>
             </div>
         );
-    }
+    };
     return (
-        
         <PrimeReactProvider>
-          <label htmlFor="dateRangePicker" className="font-bold block mb-2">
-                    Date Range
-                </label>
-                <Dropdown id="dateRangePicker" value={selectedDateRange} onChange={(e) => handleSelectedDateOption(e.value)} options={StaticDateRangeData} optionLabel="optionLabel"
-                     placeholder="Select a City" className="" 
-                     valueTemplate={dropdownValueTemplate}
-                     dropdownIcon="none"
-                     />
-           {(selectedDateRange.optionLabel === "Custom Range" && isDatePickerVisible ) && (
-            <> 
-            <Calendar
-                className="mt-2"
-                dateFormat="dd/mm/yy"
-                value={dates}
-                onChange={(e) => handleUpdateDate(e.value)}
-                selectionMode="range"
-                readOnlyInput
-                numberOfMonths={2}
-                minDate={defaultMinDate}
-                maxDate={defaultMaxDate}
-                hideOnRangeSelection
-                inline
-            />
-            </>
-                    )}
+            <label htmlFor="dateRangePicker" className="font-bold block mb-2">
+                Date Range
+            </label>
+
+            <Dropdown
+                id="dateRangePicker"
+                value={selectedDateRange}
+                onChange={(e) => handleSelectedDateOption(e.value)}
+                options={StaticDateRangeData}
+                optionLabel="optionLabel"
+                placeholder="Select a City"
+                className=""
+                valueTemplate={dropdownValueTemplate}
+                dropdownIcon="none"
+            /><br></br>
+            {selectedDateRange.optionLabel === "Custom Range" &&
+                isDatePickerVisible && (
+                    <>
+                        <Calendar
+                            dateFormat="dd/mm/yy"
+                            value={dates}
+                            onChange={(e) => handleUpdateDate(e.value)}
+                            selectionMode="range"
+                            readOnlyInput
+                            numberOfMonths={2}
+                            minDate={defaultMinDate}
+                            maxDate={defaultMaxDate}
+                            hideOnRangeSelection
+                            inline
+                        />
+                    </>
+                )}
         </PrimeReactProvider>
     );
 };
